b636f7d7e80a5e52d86927fc316d8c618796d327
tbradley <tybradleyschool@gmail.com>
2023-11-29 19:29:06 -0500

    Updated Assignment 3! Still need to implement logging properly.

--- bash/TBradleyA3.sh
+++ bash/TBradleyA3.sh
@@ -6,6 +6,7 @@
 # # Date: November 22nd, 2023
 
 # Define color variables to be used for formatting.
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -69,6 +70,21 @@ RESET='\033[0m' # Reset text formatting and color
 # Enstantiating variable names for script begins here
 #-----------------------------------------------------
 
+# Assigns the current username to the 'USERNAME' variable.
+USERNAME=$(whoami)
+
+# Saves the current date in this format: Day of the week, Month, Day, Year.
+CURRENTDATE=$(echo "$(date +%A), $(date +%B) $(date +%d), $(date +%+4Y)")
+
+# Saves the current time in this format: Hour:Minute:Second AM/PM.
+CURRENTTIME=$(echo "$(date +%I):$(date +%M):$(date +%S) $(date +%p)")
+
+# IP address for loghost
+LOGHOSTIP="192.168.16.3"
+
+# IP address for webhost
+WEBHOSTIP="192.168.16.4"
+
 # Enstantiating variable names for script ends here
 #-----------------------------------------------------
 
@@ -95,65 +111,209 @@ ${YELLOW}Time${RESET}: $CURRENTTIME
 # target1-mgmt (172.16.1.10):
 
 # Set the hostname to loghost in /etc/hostname
-ssh remoteadmin@target1-mgmt echo "loghost" > /etc/hostname
+ssh remoteadmin@target1-mgmt 'echo "loghost" > /etc/hostname'
 
 # Update the current hostname on target1
 echo -e "\n${GREEN}Updating hostname on target1...${RESET}"
-ssh remoteadmin@target1-mgmt hostnamectl set-hostname loghost
+
+ssh remoteadmin@target1-mgmt 'hostnamectl set-hostname loghost'
 
 # Display the updated hostname on target1
 echo -e "
 ${GREEN}Updated hostname on target1 to:${RESET} loghost"
 
 # Update the ip address for target1 from host 10 to host 3 on the LAN.
-ssh remoteadmin@target1-mgmt ip addr change 172.16.1.3/24 dev eth0
+ssh remoteadmin@target1-mgmt 'ip addr change 172.16.1.3/24 dev eth0'
 
-# Updated /etc/hosts file on loghost with information regarding webhost so that it is host 4 on the LAN.
-ssh remoteadmin@target1-mgmt echo '172.16.1.4 webhost' >> /etc/hosts
+# Display the updated ip address on the LAN for target 1
+echo -e "
+${GREEN}Updated ip address on loghost to:${RESET} 172.16.1.3/24 on eth0."
 
-# install ufw if necessary and allow connections to port 514/udp from the mgmt network
-ssh remoteadmin@target1-mgmt sudo apt-get update && sudo apt-get install -y ufw
-ssh remoteadmin@target1-mgmt sudo ufw allow from 172.16.1.0/24 to any port 514 proto udp
+# Updated /etc/hosts file on target1 with information regarding webhost so that it is host 4 on the LAN.
+ssh remoteadmin@target1-mgmt 'echo '172.16.1.4 webhost' >> /etc/hosts'
 
-# configure rsyslog to listen for UDP connections (look in /etc/rsyslog.conf for the configuration settings lines that say imudp, and uncomment both of them), then restart the rsyslog service using systemctl restart rsyslog
-ssh remoteadmin@target1-mgmt sudo sed -i 's/^#module(load="imudp")/module(load="imudp")/' /etc/rsyslog.conf
-ssh remoteadmin@target1-mgmt sudo sed -i 's/^#input(type="imudp" port="514")/input(type="imudp" port="514")/' /etc/rsyslog.conf
-ssh remoteadmin@target1-mgmt sudo systemctl restart rsyslog
+# Display the updated field on target1 in /etc/hosts
+echo -e "
+${GREEN}Updated /etc/hosts with webhost field:${RESET} 172.16.1.4 webhost"
+
+# Check if UFW service is running
+
+echo -e "
+${GREEN}Checking if UFW is running on loghost...${RESET}"
+
+if ! service ufw status &> /dev/null; then
+    # Run a sudo apt-get update for compatibility.
+    echo -e "
+${RED}UFW service not found on loghost; Installing.${RESET}"
+
+    ssh remoteadmin@target1-mgmt 'sudo apt-get update &> /dev/null && sudo apt-get install -y ufw &> /dev/null'
+
+    ssh remoteadmin@target1-mgmt 'sudo ufw allow from 172.16.1.0/24 to any port 514 proto udp &> /dev/null'
+
+    # Start UFW service after installation
+    ssh remoteadmin@target1-mgmt 'sudo systemctl start ufw &> /dev/null'
+else
+    # If UFW service is already running, display that message to the user.
+    echo -e "
+${BLUE}UFW service already installed on loghost; Restarting..${RESET}"
+    
+    ssh remoteadmin@target1-mgmt 'sudo ufw allow from 172.16.1.0/24 to any port 514 proto udp &> /dev/null'
+    ssh remoteadmin@target1-mgmt 'sudo ufw allow 22/tcp &> /dev/null'
+    ssh remoteadmin@target1-mgmt 'sudo systemctl restart ufw &> /dev/null'
+
+fi
+
+# configure rsyslog to listen for UDP connections (look in /etc/rsyslog.conf for the configuration settings lines that say imudp, and uncomment both of them)
+
+ssh remoteadmin@target1-mgmt "sudo sed -i 's/^#module(load=\"imudp\")/module(load=\"imudp\")/' /etc/rsyslog.conf"
+ssh remoteadmin@target1-mgmt "sudo sed -i 's/^#input(type=\"imudp\" port=\"514\")/input(type=\"imudp\" port=\"514\")/' /etc/rsyslog.conf"
+
+echo -e "
+${GREEN}Configured rsyslog on loghost to listen for UDP connections.${RESET}"
+
+#restart the rsyslog service using systemctl restart rsyslog
+
+ssh remoteadmin@target1-mgmt "sudo systemctl restart rsyslog"
+
+echo -e "
+${GREEN}Restarting rsyslog on loghost.${RESET}"
+
 
 # target2-mgmt (172.16.1.11):
 
 # Set the hostname to webhost in /etc/hostname
-ssh remoteadmin@target2-mgmt echo "webhost" > /etc/hostname
+ssh remoteadmin@target2-mgmt 'echo "webhost" > /etc/hostname'
 
 # Update the current hostname on target2
-ssh remoteadmin@target2-mgmt hostnamectl set-hostname webhost
+echo -e "\n${GREEN}Updating hostname on target2...${RESET}"
+
+# Update the current hostname on target2
+ssh remoteadmin@target2-mgmt 'hostnamectl set-hostname webhost'
 
 # Display the updated hostname on target2
 echo -e "
 ${GREEN}Updated hostname on target2 to:${RESET} webhost"
 
 # Update the ip address for target2 from host 11 to host 4 on the LAN.
-ssh remoteadmin@target2-mgmt ip addr change 172.16.1.4/24 dev eth0
+ssh remoteadmin@target2-mgmt 'ip addr change 172.16.1.4/24 dev eth0'
+
+# Display the updated ip address on the LAN for target 2
+echo -e "
+${GREEN}Updated ip address on webhost to:${RESET} 172.16.1.4/24 on eth0."
 
 # Add a machine named loghost to the /etc/hosts file as host 3 on the LAN
-ssh remoteadmin@target2-mgmt echo '172.16.1.3 loghost' >> /etc/hosts
+ssh remoteadmin@target2-mgmt 'echo '172.16.1.3 loghost' >> /etc/hosts'
 
-# Install ufw if necessary and allow connections to port 80/tcp from anywhere
-ssh remoteadmin@target2-mgmt sudo apt-get update && sudo apt-get install -y ufw
-ssh remoteadmin@target2-mgmt sudo ufw allow 80/tcp
+# Display the updated field on target2 in /etc/hosts
+echo -e "
+${GREEN}Updated /etc/hosts with loghost field:${RESET} 172.16.1.3 loghost"
+
+# Check if UFW service is running
+
+echo -e "
+${GREEN}Checking if UFW is running on webhost...${RESET}"
+
+if ! service ufw status &> /dev/null; then
+    # Run a sudo apt-get update for compatibility.
+    echo -e "
+${RED}UFW service not found on webhost; Installing.${RESET}"
+
+    ssh remoteadmin@target2-mgmt 'sudo apt-get update &> /dev/null && sudo apt-get install -y ufw &> /dev/null'
+
+    ssh remoteadmin@target2-mgmt 'sudo ufw allow 80/tcp &> /dev/null'
+
+    # Start UFW service after installation
+    ssh remoteadmin@target2-mgmt 'sudo systemctl start ufw &> /dev/null'
+else
+    # If UFW service is already running, display that message to the user.
+    echo -e "
+${BLUE}UFW service already installed on webhost; Restarting..${RESET}"
+    
+    ssh remoteadmin@target2-mgmt 'sudo ufw allow 80/tcp &> /dev/null'
+    ssh remoteadmin@target2-mgmt 'sudo ufw allow 22/tcp &> /dev/null'
+    ssh remoteadmin@target2-mgmt 'sudo systemctl restart ufw &> /dev/null'
+
+fi
 
 # Install apache2 in its default configuration
-ssh remoteadmin@target2-mgmt sudo apt-get install -y apache2
+#echo -e "${GREEN}Installing Apache2...${RESET} webhost"
+#ssh remoteadmin@target2-mgmt 'sudo apt-get install -y apache2 &> /dev/null'
+
+# Check if Apache2 web server service is running
+
+echo -e "
+${GREEN}Checking if Apache2 web service is running on webhost...${RESET}"
+
+if ! ssh remoteadmin@target2-mgmt 'systemctl is-active --quiet apache2'; then
+    
+    # Run a sudo apt-get update for compatibility.
+    echo -e "
+    ${RED}Apache2 web server service not found on webhost; Installing.${RESET}"
+    
+    ssh remoteadmin@target2-mgmt "sudo apt-get update &> /dev/null && apt-get install -y apache2 &> /dev/null"
+
+    # Start Apache2 service after installation
+    ssh remoteadmin@target2-mgmt 'sudo systemctl start apache2 &> /dev/null'
+else
+    # If Apache2 web server service is already running, display that message to the user.
+    echo -e "${BLUE}Apache2 web server service already installed on webhost; Restarting..${RESET}"
+    
+    ssh remoteadmin@target2-mgmt 'sudo systemctl restart apache2 &> /dev/null'
+fi
 
 # Configure rsyslog on webhost to send logs to loghost
-ssh remoteadmin@target2-mgmt sudo sed -i 's/^#module(load="imudp")/module(load="imudp")/' /etc/rsyslog.conf
-ssh remoteadmin@target2-mgmt sudo sed -i 's/^#input(type="imudp" port="514")/input(type="imudp" port="514")/' /etc/rsyslog.conf
-ssh remoteadmin@target2-mgmt sudo systemctl restart rsyslog
+ssh remoteadmin@target2-mgmt "sudo sed -i 's/^#module(load=\"imudp\")/module(load=\"imudp\")/' /etc/rsyslog.conf"
+ssh remoteadmin@target2-mgmt "sudo sed -i 's/^#input(type=\"imudp\" port=\"514\")/input(type=\"imudp\" port=\"514\")/' /etc/rsyslog.conf"
 
+echo -e "
+${GREEN}Configured rsyslog on webhost to send logs to loghost.${RESET}"
+
+# Configure rsyslog on webhost to send all log messages to the loghost.
+ssh remoteadmin@target2-mgmt "echo '*.* @loghost' | sudo bash -c 'cat >> /etc/rsyslog.conf'"
+
+#restart the rsyslog service using systemctl restart rsyslog
+
+ssh remoteadmin@target2-mgmt "sudo systemctl restart rsyslog"
+
+echo -e "
+${GREEN}Restarting rsyslog on webhost.${RESET}"
 
 # Update the NMS /etc/hosts file to have the name loghost with the correct IP address and the name webhost with the correct IP address.
-echo '192.168.16.3 loghost' >> /etc/hosts
-echo '192.168.16.4 webhost' >> /etc/hosts
+#sudo bash -c 'echo "192.168.16.3 loghost" >> /etc/hosts'
+#sudo bash -c 'echo "192.168.16.4 webhost" >> /etc/hosts'
+
+# Check if an entry exists in /etc/hosts for the loghost's IP.
+if grep -q "$LOGHOSTIP loghost" /etc/hosts; then
+    # If the entry exists, update it with a new host name.
+    sudo sed -i "s/^$LOGHOSTIP.*/$LOGHOSTIP loghost/" /etc/hosts
+    LOGHOSTETC=$(grep "$LOGHOSTIP loghost" /etc/hosts)
+    echo -e "
+    ${GREEN}Updated entry in /etc/hosts:${RESET} $LOGHOSTETC"
+else
+    # Entry doesn't exist, add it to the file with a host name.
+    sudo bash -c "echo '$LOGHOSTIP loghost' >> /etc/hosts"
+    LOGHOSTETC=$(grep "$LOGHOSTIP loghost" /etc/hosts)
+    echo -e "
+    ${GREEN}Added entry to /etc/hosts:${RESET} $LOGHOSTETC"
+fi
+
+# Check if an entry exists in /etc/hosts for the webhosts's IP.
+if grep -q "$WEBHOSTIP webhost" /etc/hosts; then
+    # If the entry exists, update it with a new host name.
+    sudo sed -i "s/^$WEBHOSTIP.*/$WEBHOSTIP webhost/" /etc/hosts
+    WEBHOSTETC=$(grep "$WEBHOSTIP webhost" /etc/hosts)
+    echo -e "
+    ${GREEN}Updated entry in /etc/hosts:${RESET} $WEBHOSTETC"
+else
+    # Entry doesn't exist, add it to the file with a host name.
+    sudo bash -c "echo '$WEBHOSTIP webhost' >> /etc/hosts"
+    WEBHOSTETC=$(grep "$WEBHOSTIP webhost" /etc/hosts)
+    echo -e "
+    ${GREEN}Added entry to /etc/hosts:${RESET} $WEBHOSTETC"
+fi
+
+echo -e "
+${GREEN}Updating /etc/hosts with entries for loghost and webhost.${RESET}"
 
 # Retrieve the logs showing webhost from loghost
-ssh remoteadmin@loghost grep webhost /var/log/syslog
+ssh remoteadmin@loghost "grep target2 /var/log/syslog"
